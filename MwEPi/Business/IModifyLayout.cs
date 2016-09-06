ddc17e645a244f3c1a47907e7533af4e0291e950
anders-r-lindell <anders.r.lindell@gmail.com>
2016-09-06 12:45:00 +0200

    initial commit

---
+++ MwEPi/Business/IModifyLayout.cs
@@ -0,0 +1,13 @@
+using MwEPi.Models.ViewModels;
+
+namespace MwEPi.Business
+{
+    /// <summary>
+    /// Defines a method which may be invoked by PageContextActionFilter allowing controllers
+    /// to modify common layout properties of the view model.
+    /// </summary>
+    interface IModifyLayout
+    {
+        void ModifyLayout(LayoutModel layoutModel);
+    }
+}
